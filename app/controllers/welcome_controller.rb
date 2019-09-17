a1c1c80ec0d248541f764ab72cb4325f1f08bdfe
Nick Ortiz <nickortiz@Nicks-MacBook-Pro.local>
2019-09-17 14:54:25 -0400

    fixed another typo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
     def index
-        render json: {statusL 200, message: 'donutdash leaderboard'}
+        render json: {status: 200, message: 'donutdash leaderboard'}
     end
 end
